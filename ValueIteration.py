268c383ef415b5d4bddc449391a690bca18fb818
Sahil177 <sahil0121@live.com>
2021-08-04 21:33:22 +0100

    completed value iteration

---
+++ ValueIteration.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pprint
+import sys
+if "../" not in sys.path:
+  sys.path.append("../") 
+from lib.envs.gridworld import GridworldEnv
+
+pp = pprint.PrettyPrinter(indent=2)
+env = GridworldEnv()
+
+def value_iteration(env, theta=0.0001, discount_factor=1.0):
+    """
+    Value Iteration Algorithm.
+    
+    Args:
+        env: OpenAI env. env.P represents the transition probabilities of the environment.
+            env.P[s][a] is a list of transition tuples (prob, next_state, reward, done).
+            env.nS is a number of states in the environment. 
+            env.nA is a number of actions in the environment.
+        theta: We stop evaluation once our value function change is less than theta for all states.
+        discount_factor: Gamma discount factor.
+        
+    Returns:
+        A tuple (policy, V) of the optimal policy and the optimal value function.        
+    """
+    
+
+    V = np.zeros(env.nS)
+    policy = np.zeros([env.nS, env.nA])
+
+    while True:
+        delta = 0
+        print(V)
+        for i in range(env.nS):
+            v = V[i]
+            actions = []
+            for j in range(len(policy[i])):
+                prob, next_state, reward, done = env.P[i][j][0]
+                actions.append(prob * (reward + discount_factor * V[next_state]))
+            best_action = np.argmax(actions)
+            V[i] = actions[best_action]
+            policy[i] = np.eye(env.nA)[best_action]
+            delta = max(delta,abs(v-V[i]))
+            #print(delta)
+        if delta < theta:
+            break
+    
+    
+    return policy, V
+
+
+policy, v = value_iteration(env)
+
+print("Policy Probability Distribution:")
+print(policy)
+print("")
+
+print("Reshaped Grid Policy (0=up, 1=right, 2=down, 3=left):")
+print(np.reshape(np.argmax(policy, axis=1), env.shape))
+print("")
+
+print("Value Function:")
+print(v)
+print("")
+
+print("Reshaped Grid Value Function:")
+print(v.reshape(env.shape))
+print("")
